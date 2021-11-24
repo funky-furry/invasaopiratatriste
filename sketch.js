8af488905356e6cbb4cfa21c594cd869f2fac08f
marip <mariana.silva@solucx.com.br>
2021-11-24 11:04:22 -0300

    docs: Created tower and ground

--- sketch.js
+++ sketch.js
@@ -4,10 +4,12 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 var engine, world,ground;
 
-var ground;
+var ground, tower;
+var backgroundImage, towerImage;
 
 function preload() {
- 
+  backgroundImage = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
 }
 function setup() {
 
@@ -20,13 +22,18 @@ function setup() {
   };
   ground = Bodies.rectangle(0, height - 1, width * 2, 1, options);
   World.add(world, ground);
-
+  tower = Bodies.rectangle(160, 350, 160, 310, options);
+  World.add(world, tower);
 }
 
 function draw() {
-  background(189);
+  image(backgroundImage, 0, 0, width, height);
  
   Engine.update(engine);
   
   rect(ground.position.x, ground.position.y, width*2, 1);
+  push();
+  imageMode(CENTER);
+  image(towerImage, tower.position.x, tower.position.y,160, 310);
+  pop();
 }
