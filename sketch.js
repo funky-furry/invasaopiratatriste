30c0aae2e82951dc84c1b5c5e16637b9d29238e0
marip <mariana.silva@solucx.com.br>
2021-11-24 10:48:21 -0300

    docs: Initial state

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 var engine, world,ground;
 
-
+var ground;
 
 function preload() {
  
@@ -14,8 +14,13 @@ function setup() {
   canvas = createCanvas(1200, 600);
   engine = Engine.create();
   world = engine.world;
-  
- 
+
+  options = { 
+    isStatic: true,
+  };
+  ground = Bodies.rectangle(0, height - 1, width * 2, 1, options);
+  World.add(world, ground);
+
 }
 
 function draw() {
@@ -23,5 +28,5 @@ function draw() {
  
   Engine.update(engine);
   
-   
+  rect(ground.position.x, ground.position.y, width*2, 1);
 }
